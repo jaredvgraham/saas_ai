66876b5958df688e8267f1d8e8577697750341d2
Jared Graham <jaredgraham@jareds-mbp.mynetworksettings.com>
2024-06-25 19:16:10 -0400

    clerk webhooks

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -5,10 +5,11 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   photo: { type: String, required: true },
-  fistName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, default: "" }, // fixed typo and added default value
+  lastName: { type: String, default: "" }, // added default value
   planId: { type: String },
   creditBalance: { type: Number, default: 10 },
 });
+
 const User = models.User || model("User", UserSchema);
 export default User;
